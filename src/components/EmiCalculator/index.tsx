8f0978b3e0fade86cf934717b6e3d90d342cfa49
Pankaj Gamnani <pankaj.gamnani@cimpress.com>
2022-01-30 21:38:08 +0530

    Adding support to save loanDetails offline and reset functionality

--- src/components/EmiCalculator/index.tsx
+++ src/components/EmiCalculator/index.tsx
@@ -1,34 +1,61 @@
 import { useState } from "react";
-import { MortgageInput } from "./MortgageInput";
 import { calculateEmi } from 'utils/calculate-emi';
+import { getLoanDetails, resetLoanDetails, saveLoanDetails, TLoanDetails } from "utils/loan-details-storage-service";
+import { MortgageInput } from "./MortgageInput";
 import { useEffect } from "react";
 import { Amortization } from "./Amortization";
 import { Emi } from "./Emi";
 
+export type TEmiCalculatorState = {
+    amount: number;
+    interest: number;
+    months: number;
+};
+
 export const EmiCalculator = () => {
-    const [amount, setAmount] = useState<number>(1000000);
-    const [interest, setInterest] = useState<number>(10);
-    const [months, setMonths] = useState<number>(120);
+    const initialState: TEmiCalculatorState = { amount: 1000000, interest: 10, months: 120 };
+
+    const [loanDetails, setLoanDetails] = useState<TLoanDetails>(initialState);
+    const setAmount = (newAmount: number) => setLoanDetails({ ...loanDetails, amount: newAmount });
+    const setInterest = (newInterest: number) => setLoanDetails({ ...loanDetails, interest: newInterest });
+    const setMonths = (newMonths: number) => setLoanDetails({ ...loanDetails, months: newMonths });
     const [emi, setEmi] = useState<number>(0);
 
     useEffect(() => {
-        setEmi(calculateEmi({ amount, interest, months }));
-    }, [amount, interest, months]);
+        const savedLoanDetails: TLoanDetails | null = getLoanDetails();
+
+        if (!!savedLoanDetails) {
+            setLoanDetails(savedLoanDetails);
+        }
+    }, []);
+
+    useEffect(() => {
+        saveLoanDetails(loanDetails);
+        setEmi(calculateEmi(loanDetails));
+    }, [loanDetails, loanDetails.amount, loanDetails.interest, loanDetails.months]);
+
+    const ResetLoanDetails = () => {
+        const handleClick = () => {
+            resetLoanDetails();
+            setLoanDetails(initialState);
+        };
+        return (
+            <p className="absolute translate-x-14 translate-y-16 text-xs text-blue-600 cursor-pointer" onClick={handleClick}>
+                reset &#x21bb;
+            </p>
+        );
+    };
 
     return (
         <div id="emi-calculator" className="mt-8">
             <div id="emi-inputs" className="flex flex-col justify-center items-center">
-                <MortgageInput label="Amount" value={amount} step={100000} setValue={setAmount} />
-                <MortgageInput label="Interest" value={interest} step={0.05} setValue={setInterest} />
-                <MortgageInput label="Months" value={months} step={10} setValue={setMonths} />
+                <MortgageInput label="Amount" value={loanDetails.amount} step={100000} setValue={setAmount} />
+                <MortgageInput label="Interest" value={loanDetails.interest} step={0.05} setValue={setInterest} />
+                <MortgageInput label="Months" value={loanDetails.months} step={10} setValue={setMonths} />
+                <ResetLoanDetails />
                 <Emi emi={emi} />
             </div>
-            <Amortization
-                amount={amount}
-                interest={interest}
-                months={months}
-                emi={emi}
-            />
+            <Amortization {...loanDetails} emi={emi} />
         </div>
     );
 };
---
+++ src/utils/loan-details-storage-service.ts
@@ -0,0 +1,21 @@
+export type TLoanDetails = {
+    amount: number;
+    interest: number;
+    months: number;
+};
+
+const LOAN_DETAILS_STORAGE_KEY_NAME = 'emiCalculator:LoanDetails';
+
+export const getLoanDetails = (): TLoanDetails | null => {
+    const loanDetails = localStorage.getItem(LOAN_DETAILS_STORAGE_KEY_NAME);
+
+    return loanDetails ? JSON.parse(loanDetails) : null;
+};
+
+export const saveLoanDetails = (loanDetails: TLoanDetails) => {
+    localStorage.setItem(LOAN_DETAILS_STORAGE_KEY_NAME, JSON.stringify(loanDetails));
+};
+
+export const resetLoanDetails = () => {
+    localStorage.removeItem(LOAN_DETAILS_STORAGE_KEY_NAME);
+}
