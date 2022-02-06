6f0b4b21e30c4f1c52d8530a31a954ea0d716f66
Pankaj Gamnani <pankaj.gamnani@cimpress.com>
2022-02-06 21:40:07 +0530

    Adding collapsible table for monthly amortization data

--- src/components/EmiCalculator/Amortization.tsx
+++ src/components/EmiCalculator/Amortization.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import { TAmortizationSummary } from 'utils/calculate-amortization';
+import React, { useState } from 'react';
+import { TAmoritzation, TAmortizationSummary } from 'utils/calculate-amortization';
+
+type TAmortizationGroupProps = {
+  isHidden: boolean;
+  group: TAmoritzation
+};
+function AmortizationGroup({ isHidden, group }: TAmortizationGroupProps) {
+  return (
+    <td colSpan={6} className="p-0" hidden={isHidden}>
+      <table className="w-full">
+        <tbody>
+          {
+            group.map((amortizationEntry) => (
+              <tr key={amortizationEntry.index}>
+                <td className="text-sm">&and;</td>
+                <td>{amortizationEntry.monthName}</td>
+                <td>{Math.round(amortizationEntry.amount)}</td>
+                <td className="text-red-700">{Math.round(amortizationEntry.interest)}</td>
+                <td className="text-green-700">{Math.round(amortizationEntry.principal)}</td>
+                <td>{Math.round(amortizationEntry.pending)}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </td>
+  );
+}
 
 function AmortizationHeader() {
   return (
     <thead id="amortization-header" className="sticky top-0 bg-white">
-      <tr className="text-center">
+      <tr>
+        <td className="sticky top-0" />
         <td className="sticky top-0">Date</td>
         <td className="sticky top-0">Amount</td>
         <td className="sticky top-0">Interest</td>
@@ -20,21 +48,26 @@ type TAmortizationBodyProps = {
 };
 function AmortizationBody({ amortizationSummary }: TAmortizationBodyProps) {
   const years: string[] = Object.keys(amortizationSummary);
+  const [isHidden, setIsHidden] = useState<boolean>(true);
 
   return (
-    <tbody id="amortization-body">
+    <tbody id="amortization-body" className="font-lato font-light">
       {
         years.map((year) => {
           const summary = amortizationSummary[year];
-          return (
-            <tr key={summary.year} className="font-lato font-light text-center">
+          return [
+            <tr key={summary.year} className="cursor-pointer" onClick={() => setIsHidden(!isHidden)}>
+              <td className="text-sm">&or;</td>
               <td>{summary.year}</td>
               <td>{Math.round(summary.amount)}</td>
-              <td className="text-red-600">{Math.round(summary.interest)}</td>
-              <td className="text-green-600">{Math.round(summary.principal)}</td>
+              <td className="text-red-700">{Math.round(summary.interest)}</td>
+              <td className="text-green-700">{Math.round(summary.principal)}</td>
               <td>{Math.round(summary.pending)}</td>
-            </tr>
-          );
+            </tr>,
+            <tr key={`amortization-group-${summary.year}`}>
+              <AmortizationGroup group={summary.group} isHidden={isHidden} />
+            </tr>,
+          ];
         })
       }
     </tbody>
@@ -49,7 +82,7 @@ export function Amortization({ amortizationSummary }: TAmoritzationProps) {
     <div id="amortization" className="mt-8 grid justify-items-center">
       <h3 className="pb-2">Amortization Schedule</h3>
       <div className="w-full px-8 md:w-1/2 max-h-96 overflow-y-auto border rounded-lg">
-        <table className="w-full text-right border-separate">
+        <table className="w-full text-center">
           <AmortizationHeader />
           <AmortizationBody amortizationSummary={amortizationSummary} />
         </table>
